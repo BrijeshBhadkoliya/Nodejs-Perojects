2ad7f4016d58cdbe95b583c0e3b127907fe8c8c4
BrijeshBhadkoliya <bhadkoliyajbrijesh@gmail.com>
2024-10-24 16:16:24 +0530

    All projects

---
+++ Pr.4 Book_store/index.js
@@ -0,0 +1,77 @@
+const express = require('express');
+
+const port = 7200;
+require('./config/db')
+const app = express()
+const mongoose = require('mongoose');
+const UserModel = require('./modules/BookModels');
+
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", async (req, res) => {
+
+    let data = await UserModel.find({})
+    return res.render("veiw", { data })
+})
+
+app.get('/add', (req, res) => {
+    return res.render('add');
+});
+app.post('/addData', (req, res) => {
+    const { name, price, pages, description } = req.body;
+    UserModel.create({
+        name,
+        price,
+        pages,
+        description
+    }).then((data, err) => {
+        if (err) {
+            console.log(err);
+            return false;
+        }
+        console.log('record add');
+        return res.redirect('/');
+
+    })
+
+});
+
+app.get('/deletRecord', (req, res) => {
+    let id = new mongoose.Types.ObjectId(req.query.id);
+    // console.log("id ====> ", id);
+    UserModel.findByIdAndDelete(id).then((data) => {
+        console.log("user delete");
+        return res.redirect('/')
+    }).catch((err) => {
+        console.log(err);
+        return false;
+    })
+})
+
+app.get('/editData', async (req, res) => {
+    const id = new mongoose.Types.ObjectId(req.query.id)
+    const data = await UserModel.findById(id)
+    return res.render('edit', { data })
+})
+
+
+app.post('/Upgreat', async (req, res) => {
+    const {editId , name , price , pages , description} = req.body
+  await  UserModel.findByIdAndUpdate(editId,{
+        name,
+        price,
+        pages,
+        description
+    })
+    return res.redirect('/')
+})
+
+app.listen(port, (err) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(`server is runing http://localhost:${port}/add `);
+
+}); 
