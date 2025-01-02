eee4e64a56a183c240f797cc48c2c63c949a720c
BrijeshBhadkoliya <bhadkoliyajbrijesh@gmail.com>
2025-01-02 14:59:45 +0530

    p

--- Pr.9 Api-project/routes/authroutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { loginUser } = require('../controller/authcontroller');  
-
-const routes = express.Router();
-
-routes.post('/login', loginUser); 
-
-module.exports = routes;
